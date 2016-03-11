f0941e378bfc258e3ea3d0d88f5a7837c115d6cc
Carles <carlos.saport@gmail.com>
2016-03-11 15:25:37 +0100

    register i cron

--- src/DropstockBundle/Form/SiteType.php
+++ src/DropstockBundle/Form/SiteType.php
@@ -8,27 +8,29 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SiteType extends AbstractType
 {
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('name')
-            ->add('url')
-            ->add('software')
-            ->add('status')
-        ;
-    }
-    
-    /**
-     * @param OptionsResolver $resolver
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'DropstockBundle\Entity\Site'
-        ));
-    }
+  /**
+   * @param FormBuilderInterface $builder
+   * @param array $options
+   */
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+      ->add('name')
+      ->add('url')
+      ->add('platform')
+      ->add('status')
+      ->add('checked')
+      ->add('token')
+      ;
+  }
+  
+  /**
+   * @param OptionsResolver $resolver
+   */
+  public function configureOptions(OptionsResolver $resolver)
+  {
+    $resolver->setDefaults(array(
+                             'data_class' => 'DropstockBundle\Entity\Site'
+                           ));
+  }
 }
