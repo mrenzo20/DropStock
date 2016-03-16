685a6604923c3270fcbfe5f529f4bde63e5974b8
Carles <carlos.saport@gmail.com>
2016-03-16 18:40:41 +0100

    moure la funció

--- src/DropstockBundle/Controller/SiteController.php
+++ src/DropstockBundle/Controller/SiteController.php
@@ -329,41 +329,6 @@ class SiteController extends Controller
     }
 
 
-  /**
-   * simple method to encrypt or decrypt a plain text string
-   * initialization vector(IV) has to be the same when encrypting and decrypting
-   * PHP 5.4.9
-   *
-   * this is a beginners template for simple encryption decryption
-   * before using this in production environments, please read about encryption
-   *
-   * @param string $action: can be 'encrypt' or 'decrypt'
-   * @param string $string: string to encrypt or decrypt
-   *
-   * @return string
-   */
-  public function encrypt_decrypt($action, $string, $secret) {
-    $output = false;
-
-    $encrypt_method = "AES-256-CBC";
-    $secret_key = $secret;
-    $secret_iv = ''; //salt
-
-    // hash
-    $key = hash('sha256', $secret_key);
-
-    // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
-    $iv = substr(hash('sha256', $secret_iv), 0, 16);
-
-    if( $action == 'encrypt' ) {
-      $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
-      $output = base64_encode($output);
-    }
-    else if( $action == 'decrypt' ){
-      $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
-    }
-
-    return $output;
-  }
+  
   
 }
