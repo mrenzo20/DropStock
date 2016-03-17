5df8b0329d777ea85254be0bc7db26591830f2d5
Carles <carlos.saport@gmail.com>
2016-03-17 18:06:15 +0100

    typpo

--- src/DropstockBundle/Form/SiteType.php
+++ src/DropstockBundle/Form/SiteType.php
@@ -24,16 +24,16 @@ class SiteType extends AbstractType
       ->add('checked')
       ->add('token')
       ->add('crypt')
-      ->add('modules',
-            CollectionType::class,
-            array(
-              'entry_type'   => TextType::class,
-              'entry_options'  => array(
-                'required'  => true,
-                'attr'      => array('class' => 'email-box')
-              ),
-            )
-      )
+      // ->add('modules',
+      //       CollectionType::class,
+      //       array(
+      //         'entry_type'   => TextType::class,
+      //         'entry_options'  => array(
+      //           'required'  => true,
+      //           'attr'      => array('class' => 'email-box')
+      //         ),
+      //       )
+      // )
       
       ->add('data')
       ;
