eb0f654d14f6ea5b530d855494e90a5219f2f44e
Carles <carlos.saport@gmail.com>
2016-03-18 15:24:52 +0100

    user bundle i altres coses

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new AppBundle\AppBundle(),
             new DropstockBundle\DropstockBundle(),
+            new UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+  public function getParent()
+  {
+    return 'FOSUserBundle';
+  }
+}
